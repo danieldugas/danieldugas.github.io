acc1a5b71e5660d151477b21ca1389475cbc7959
Daniel Dugas <daniel@dugas.ch>
2026-02-15 00:02:52 -0800

    remove before flight

--- hyperhell/game_manager/the_bargain_manager.js
+++ hyperhell/game_manager/the_bargain_manager.js
@@ -2271,7 +2271,7 @@ export class TheBargainManager {
 
             // DEBUG Remove before flight: debugging quick init
             // -----
-            if (true) { // NOCOMMIT
+            if (false) {
                 this.gameState.GOD_MODE = true;
                 document.getElementById('god_mode_checkbox').checked = true;
                 // Bargain accepted
